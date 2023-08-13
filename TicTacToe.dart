1af3144b472f183f5fa626f9486738613c4e0056
SujayR23 <138759410+SujayR23@users.noreply.github.com>
2023-08-13 09:33:34 +0530

    Update TicTacToe.dart

--- TicTacToe.dart
+++ TicTacToe.dart
@@ -1,1 +1,254 @@
+// <--------Main Page-------->
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tic Tac Toe',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: HomePage(),
+    );
+  }
+}
+
+// <--------- Home Page---------->
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isCross = true;
+  String message = "";
+  List<String> gameState = [];
+
+  AssetImage cross = AssetImage("images/R.png");
+  AssetImage circle = AssetImage("images/o.png");
+  AssetImage edit = AssetImage("images/OIP.jpeg");
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      this.gameState = [
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+      ];
+      this.message = "";
+    });
+  }
+
+  playGame(int index) {
+    if (this.gameState[index] == "empty") {
+      setState(() {
+        if (this.isCross) {
+          this.gameState[index] = "cross";
+        } else {
+          this.gameState[index] = "circle";
+        }
+        this.isCross = !this.isCross;
+        this.checkWin();
+      });
+    }
+  }
+
+  resetGame() {
+    setState(() {
+      this.gameState = [
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+      ];
+      this.message = "";
+    });
+  }
+
+  getImage(String value) {
+    switch (value) {
+      case ('empty'):
+        return edit;
+        break;
+      case ('cross'):
+        return cross;
+        break;
+      case ('circle'):
+        return circle;
+        break;
+    }
+  }
+
+  checkWin() {
+    if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[1]) &&
+        (gameState[1] == gameState[2])) {
+      setState(() {
+        this.message = '${this.gameState[0]} wins';
+      });
+      Future.delayed(Duration(seconds: 4), () {
+        this.resetGame();
+        this.getImage("empty");
+      });
+    } else if ((gameState[3] != 'empty') &&
+        (gameState[3] == gameState[4]) &&
+        (gameState[4] == gameState[5])) {
+      setState(() {
+        this.message = '${this.gameState[3]} wins';
+      });
+      Future.delayed(Duration(seconds: 4), () {
+        this.resetGame();
+      });
+    } else if ((gameState[6] != 'empty') &&
+        (gameState[6] == gameState[7]) &&
+        (gameState[7] == gameState[8])) {
+      setState(() {
+        this.message = '${this.gameState[6]} wins';
+      });
+      Future.delayed(Duration(seconds: 4), () {
+        this.resetGame();
+      });
+    } else if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[3]) &&
+        (gameState[3] == gameState[6])) {
+      setState(() {
+        this.message = '${this.gameState[0]} wins';
+      });
+      Future.delayed(Duration(seconds: 4), () {
+        this.resetGame();
+      });
+    } else if ((gameState[1] != 'empty') &&
+        (gameState[1] == gameState[4]) &&
+        (gameState[4] == gameState[7])) {
+      setState(() {
+        this.message = '${this.gameState[1]} wins';
+      });
+      Future.delayed(Duration(seconds: 4), () {
+        this.resetGame();
+      });
+    } else if ((gameState[2] != 'empty') &&
+        (gameState[2] == gameState[5]) &&
+        (gameState[5] == gameState[8])) {
+      setState(() {
+        this.message = '${this.gameState[2]} wins';
+      });
+      Future.delayed(Duration(seconds: 4), () {
+        this.resetGame();
+      });
+    } else if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[4]) &&
+        (gameState[4] == gameState[8])) {
+      setState(() {
+        this.message = '${this.gameState[0]} wins';
+      });
+      Future.delayed(Duration(seconds: 4), () {
+        this.resetGame();
+      });
+    } else if ((gameState[2] != 'empty') &&
+        (gameState[2] == gameState[4]) &&
+        (gameState[4] == gameState[6])) {
+      setState(() {
+        this.message = '${this.gameState[2]} wins';
+      });
+      Future.delayed(Duration(seconds: 4), () {
+        this.resetGame();
+      });
+    } else if (!gameState.contains('empty')) {
+      setState(() {
+        this.message = "Game Draw";
+      });
+      Future.delayed(Duration(seconds: 4), () {
+        this.resetGame();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tic Tac Toe'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: GridView.builder(
+                padding: EdgeInsets.all(20.0),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 20.0,
+                ),
+                itemCount: this.gameState.length,
+                itemBuilder: (context, i) => SizedBox(
+                      width: 150.0,
+                      height: 150.0,
+                      child: MaterialButton(
+                        onPressed: () {
+                          this.playGame(i);
+                        },
+                        child: Image(
+                          image: this.getImage(this.gameState[i]),
+                        ),
+                      ),
+                    )),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              this.message,
+              style: TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          MaterialButton(
+            color: Colors.purple,
+            minWidth: 300.0,
+            height: 50.0,
+            child: Text(
+              "Reset Game",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+            onPressed: () {
+              this.resetGame();
+            },
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              "Done by Sujay",
+              style: TextStyle(fontSize: 10.0),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
